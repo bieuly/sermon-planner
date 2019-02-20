c0d0d1d7d74e173f623cfe1149d6f24a0e19353f
Billy Hoang <billy-hoang@hotmail.com>
2019-02-20 00:04:10 -0800

    Add semantic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import "./App.css";
 
 class App extends Component {
   public render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Card>
+          <Card.Content>Hello there</Card.Content>
+        </Card>
+      </div>
+    );
   }
 }
 
