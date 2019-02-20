e78914a60091807710cafcf60310145b95e08f9c
Billy Hoang <billy-hoang@hotmail.com>
2019-02-20 01:32:47 -0800

    Added CreateSermonPage

--- src/pages/CreateSermonPage/CreateSermonPage.tsx
+++ src/pages/CreateSermonPage/CreateSermonPage.tsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { Card, Grid, Icon } from "semantic-ui-react";
 
 class CreateNewSermonPage extends Component {
   public render() {
-    return <div className="create-new-sermon">Create new sermon page</div>;
+    return (
+      <Grid columns="equal">
+        <Grid.Column width={5}>
+          <Card className="new-slide-card">
+            <Card.Content header="New Slide" textAlign="center" />
+            <Card.Content textAlign="center">
+              <Icon name="plus circle" size="big" color="blue" />
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column />
+      </Grid>
+    );
   }
 }
 
