97db3a9ae4e5b6bc9dbbcc8c9d00c0a7f25445cf
Billy Hoang <billy-hoang@hotmail.com>
2019-02-19 23:57:49 -0800

    Setup tslint and prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
+  public render() {
+    return <div className="App" />;
   }
 }
 
