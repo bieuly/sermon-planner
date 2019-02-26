68808f1cc54e50b21819027c06cfb4515c4eedbd
Billy <bihoang@bestbuycanada.ca>
2019-02-26 15:14:21 -0800

    Refactor state to be more generic

--- src/components/CreateNewSlideModal/CreateNewSlideModal.tsx
+++ src/components/CreateNewSlideModal/CreateNewSlideModal.tsx
@@ -1,29 +1,63 @@
-import React, { SyntheticEvent, useState } from "react";
-import { Button, Dropdown, DropdownProps, Modal } from "semantic-ui-react";
+import React, { SyntheticEvent, useState, FunctionComponent } from "react";
+import { Button, Dropdown, DropdownProps, Modal, Form, Label } from "semantic-ui-react";
 import { SlideTypes } from "../../models/Slide";
 
 export interface IProps {
   onCreateSlide: (slideType: SlideTypes) => void;
 }
 
-const CreateNewSlideModal = ({ onCreateSlide }: IProps) => {
+export interface IState {
+  selectedSlideType: SlideTypes | null;
+}
+
+type StateKeys = keyof IState;
+
+const CreateNewSlideModal: FunctionComponent<IProps> = ({ onCreateSlide }) => {
   const [modalOpen, setModalOpen] = useState(false);
-  const [selectedSlideType, setSelectedSlideType] = useState<
-    undefined | SlideTypes
-  >(undefined);
+  const [fields, setFields] = useState<IState>({
+    selectedSlideType: null,
+  });
 
   const showCreateSlideMenu = (e: SyntheticEvent, data: any): void => {
     e.preventDefault();
     setModalOpen(true);
   };
+
+  const closeModal = () => {
+    clearFields();
+    setModalOpen(false);
+  }
+
   const closeCreateSlideMenu = (e: SyntheticEvent, data: any): void => {
     e.preventDefault();
-    setModalOpen(false);
+    closeModal();
   };
+
   const handleCreateSlide = (e: SyntheticEvent, data: any): void => {
     e.preventDefault();
-    onCreateSlide(data.id as SlideTypes);
-    setModalOpen(false);
+    if (isRequiredFieldsFilledOut()) {
+      const {selectedSlideType} = fields;
+      onCreateSlide(selectedSlideType as SlideTypes);
+      closeModal();
+    }
+  };
+
+  const clearFields = () => {
+    for(let field in fields) {
+      fields[field as StateKeys] = null;
+    }
+  }
+
+  const isRequiredFieldsFilledOut = (): boolean => {
+    return !Object.values(fields).some((field) => !field);
+  }
+
+  const slideTypeOnChange = (e: SyntheticEvent, data: DropdownProps): void => {
+    e.preventDefault();
+    setFields({
+      ...fields,
+      selectedSlideType: data.value as SlideTypes,
+    });
   };
 
   const slideTypeOptions = [
@@ -37,31 +71,33 @@ const CreateNewSlideModal = ({ onCreateSlide }: IProps) => {
     },
   ];
 
-  const slideTypeOnChange = (e: SyntheticEvent, data: DropdownProps): void => {
-    e.preventDefault();
-    setSelectedSlideType(data.value as SlideTypes);
-  };
-
   return (
-    <Modal
-      trigger={<Button onClick={showCreateSlideMenu}>Create New Slide</Button>}
+    <Form>
+      <Modal
+      trigger={<Button onClick={showCreateSlideMenu} color="blue">Create New Slide</Button>}
       open={modalOpen}
       centered={false}
       onClose={closeCreateSlideMenu}
-    >
-      <Modal.Header>Create New Slide</Modal.Header>
-      <Modal.Content>
-        <Modal.Description>
-          <p>What kind of Slide do you want to create?</p>
-        </Modal.Description>
-        <Dropdown
-          onChange={slideTypeOnChange}
-          placeholder="Select Type"
-          options={slideTypeOptions}
-          selection
-        />
-      </Modal.Content>
-    </Modal>
+      >
+        <Modal.Header>Create New Slide</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <p>What kind of Slide do you want to create?</p>
+          </Modal.Description>
+          <Form.Field>
+            <Dropdown
+              onChange={slideTypeOnChange}
+              placeholder="Select Type"
+              options={slideTypeOptions}
+              selection
+            />
+          </Form.Field>
+        </Modal.Content>
+        <Modal.Actions>
+            <Button onClick={handleCreateSlide} color="blue" active={false}>Create</Button>
+          </Modal.Actions>
+      </Modal>
+    </Form>
   );
 };
 
