ce7ca6a0c6d0328148ba250fe750d24c0b74bbc2
Billy Hoang <billy-hoang@hotmail.com>
2019-02-25 23:02:58 -0800

    Change slideType selection to dropdown

--- src/components/CreateNewSlideModal/CreateNewSlideModal.tsx
+++ src/components/CreateNewSlideModal/CreateNewSlideModal.tsx
@@ -1,5 +1,5 @@
 import React, { SyntheticEvent, useState } from "react";
-import { Button, Icon, Modal } from "semantic-ui-react";
+import { Button, Dropdown, DropdownProps, Modal } from "semantic-ui-react";
 import { SlideTypes } from "../../models/Slide";
 
 export interface IProps {
@@ -8,19 +8,40 @@ export interface IProps {
 
 const CreateNewSlideModal = ({ onCreateSlide }: IProps) => {
   const [modalOpen, setModalOpen] = useState(false);
-  const showCreateSlideMenu = (e: SyntheticEvent, data: any) => {
+  const [selectedSlideType, setSelectedSlideType] = useState<
+    undefined | SlideTypes
+  >(undefined);
+
+  const showCreateSlideMenu = (e: SyntheticEvent, data: any): void => {
     e.preventDefault();
     setModalOpen(true);
   };
-  const closeCreateSlideMenu = (e: SyntheticEvent, data: any) => {
+  const closeCreateSlideMenu = (e: SyntheticEvent, data: any): void => {
     e.preventDefault();
     setModalOpen(false);
   };
-  const handleCreateSlide = (e: SyntheticEvent, data: any) => {
+  const handleCreateSlide = (e: SyntheticEvent, data: any): void => {
     e.preventDefault();
     onCreateSlide(data.id as SlideTypes);
     setModalOpen(false);
   };
+
+  const slideTypeOptions = [
+    { key: 1, text: "Biblical", value: SlideTypes.BIBLEREF, icon: "book" },
+    { key: 2, text: "Lyrical", value: SlideTypes.LYRICAL, icon: "music" },
+    {
+      icon: "sticky note outline",
+      key: 3,
+      text: "Basic",
+      value: SlideTypes.BASIC,
+    },
+  ];
+
+  const slideTypeOnChange = (e: SyntheticEvent, data: DropdownProps): void => {
+    e.preventDefault();
+    setSelectedSlideType(data.value as SlideTypes);
+  };
+
   return (
     <Modal
       trigger={<Button onClick={showCreateSlideMenu}>Create New Slide</Button>}
@@ -33,31 +54,12 @@ const CreateNewSlideModal = ({ onCreateSlide }: IProps) => {
         <Modal.Description>
           <p>What kind of Slide do you want to create?</p>
         </Modal.Description>
-        <Button.Group>
-          <Button
-            id={SlideTypes.LYRICAL}
-            onClick={handleCreateSlide}
-            color="blue"
-          >
-            <Icon name="music" /> Lyrical
-          </Button>
-          <Button.Or />
-          <Button
-            id={SlideTypes.BIBLEREF}
-            onClick={handleCreateSlide}
-            color="green"
-          >
-            <Icon name="book" /> Biblical
-          </Button>
-          <Button.Or />
-          <Button
-            id={SlideTypes.BASIC}
-            onClick={handleCreateSlide}
-            color="olive"
-          >
-            <Icon name="sticky note outline" /> Basic
-          </Button>
-        </Button.Group>
+        <Dropdown
+          onChange={slideTypeOnChange}
+          placeholder="Select Type"
+          options={slideTypeOptions}
+          selection
+        />
       </Modal.Content>
     </Modal>
   );
